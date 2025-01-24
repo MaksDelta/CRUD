c67860bf5efb71de250c5e9779af926c8eb1e871
Maks Delta <mc.pullupenko@gmail.com>
2025-01-24 19:44:09 +0200

    added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [
+    UsersListComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
 })
 export class AppComponent {
-  title = 'crud';
+  title = 'crud-app';
+
+  constructor(private _dialog: MatDialog) {}
+
+  openAddUserForm(): void {
+    this._dialog.open(AddUserComponent);
+  }
 }
